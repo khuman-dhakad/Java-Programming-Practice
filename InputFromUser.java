f585c0084567485fb4618007bea481eab03b5759
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-21 20:49:05 +0530

    Make Formating Good In Program

--- InputFromUser.java
+++ InputFromUser.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public class InputFromUser{
-    public static void main(String[]args){
+
+public class InputFromUser {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter Your Name:");
         String name = sc.nextLine();
 
-        System.out.println("Your Name Is:"+name);
-       
+        System.out.println("Your Name Is:" + name);
+
         sc.close();
     }
 }
