fe4d028a589f27607149ba2a339924a40e809394
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-21 21:02:07 +0530

    Add Program for taking Age

--- InputFromUser.java
+++ InputFromUser.java
@@ -7,7 +7,14 @@ public class InputFromUser {
         System.out.print("Enter Your Name:");
         String name = sc.nextLine();
 
+        
+        System.out.print("Enter Your Age:");
+        int age= sc.nextInt();
+        
         System.out.println("Your Name Is:" + name);
+         System.out.println("Your Age Is:"+age);
+
+
 
         sc.close();
     }
