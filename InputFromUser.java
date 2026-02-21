101ac1296cd9a6d06b02fa24f571abb84710bfde
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-21 20:43:44 +0530

    Add USer Input Program

---
+++ InputFromUser.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class InputFromUser{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Your Name:");
+        String name = sc.nextLine();
+
+        System.out.println("Your Name Is:"+name);
+       
+        sc.close();
+    }
+}
