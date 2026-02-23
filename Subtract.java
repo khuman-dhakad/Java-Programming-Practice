b08c3ac079a7e1f5867f508fbfd240ce3c691d61
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-23 20:39:33 +0530

    Add Formatting in Program

--- Subtract.java
+++ Subtract.java
@@ -3,10 +3,10 @@ public class Subtract{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter First Number");
+        System.out.print("Enter First Number:");
         int a = sc.nextInt();
 
-        System.out.print("Enter Second Number");
+        System.out.print("Enter Second Number:");
         int b = sc.nextInt();
 
         System.out.println(a-b);
