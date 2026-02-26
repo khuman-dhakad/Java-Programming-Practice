1275b0281bfc9248e2844bd4845b76396cfc01ac
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-26 20:31:37 +0530

    Handle edge cases for Division Function

--- Calculator.java
+++ Calculator.java
@@ -2,43 +2,49 @@
 import java.util.Scanner;
 
 class Calculator {
-    public static int calculateSum(int a,int b) {
-        int sum = a+b;
+    public static int calculateSum(int a, int b) {
+        int sum = a + b;
         return sum;
     }
-    public static int calculateDiff(int a,int b) {
-        int diff = a-b;
+
+    public static int calculateDiff(int a, int b) {
+        int diff = a - b;
         return diff;
     }
-    public static int calculateMul(int a,int b) {
-        int mul = a*b;
+
+    public static int calculateMul(int a, int b) {
+        int mul = a * b;
         return mul;
     }
-    public static float calculateDiv(int a,int b) {
-        float div = (float)a/b;
+
+    public static float calculateDiv(int a, int b) {
+        if (b == 0) {
+            System.out.println("Can Not Divided By 0!");
+            return 0;
+        }
+        float div = (float) a / b;
         return div;
     }
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Number:");
-        int a= sc.nextInt();
+        int a = sc.nextInt();
         System.out.print("Enter First Number:");
-        int b= sc.nextInt();
- 
-        int res=calculateSum(a,b);
-        System.out.println("Result Is: "+res);
- 
-        int diff=calculateMul(a,b);
-        System.out.println("Result Is: "+diff);
-        
- 
-        int mul=calculateDiff(a,b);
-        System.out.println("Result Is: "+mul);
- 
-        float div=calculateDiv(a,b);
-        System.out.println("Result Is: "+div);
-        
+        int b = sc.nextInt();
+
+        int res = calculateSum(a, b);
+        System.out.println("Addition Is: " + res);
+
+        int diff = calculateMul(a, b);
+        System.out.println("Diffrence Is: " + diff);
+
+        int mul = calculateDiff(a, b);
+        System.out.println("Multiplication Is: " + mul);
+
+        float div = calculateDiv(a, b);
+        System.out.println("Division Is: " + div);
+
     }
 }
