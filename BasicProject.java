dfc31079f2c3e9c70406781636616fdaaafd74eb
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-27 20:00:35 +0530

    Creating Menu Driven Basic Project

---
+++ BasicProject.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class BasicProject{
+    public static void main(String[]args){
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("-----------------------");
+       System.out.println("Welcome to My Calculator");
+       System.out.println("-----------------------");
+       sc.close();
+    }
+}
