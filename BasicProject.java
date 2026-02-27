7b14c9b9c0b93eb2407c2ed565bbda2afd24df52
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-27 20:06:42 +0530

    Create Menu Drive In Project

--- BasicProject.java
+++ BasicProject.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
-public class BasicProject{
-    public static void main(String[]args){
-       Scanner sc = new Scanner(System.in);
 
-       System.out.println("-----------------------");
-       System.out.println("Welcome to My Calculator");
-       System.out.println("-----------------------");
-       sc.close();
+public class BasicProject {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int choice = 0;
+
+        do {
+            System.out.println("-----------------------");
+            System.out.println("Welcome to My Calculator");
+            System.out.println("-----------------------");
+
+            System.out.println("1.Addition");
+            System.out.println("1.Subtract");
+            System.out.println("1.Multiplication");
+            System.out.println("1.Division");
+            System.out.println("1.Exit");
+
+            System.out.println("\nEnter Your Choice:");
+            choice = sc.nextInt();
+
+        } while (choice != 5);
+
+        sc.close();
     }
 }
