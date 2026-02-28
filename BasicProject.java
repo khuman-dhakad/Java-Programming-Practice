12c7a3e012cbe71d46c432b2f2ec2498f756e851
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-02-28 20:52:53 +0530

    Add Addition Fubction In Project

--- BasicProject.java
+++ BasicProject.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class BasicProject {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void calculateSum() {
 
+        System.out.print("Enter First Number:");
+        int a = sc.nextInt();
+
+        System.out.print("Enter Second Number:");
+        int b = sc.nextInt();
+
+        int res = a + b;
+        System.out.println("Result Is:" + res);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         int choice = 0;
 
         do {
@@ -21,26 +33,25 @@ public class BasicProject {
             System.out.print("\nEnter Your Choice:");
             choice = sc.nextInt();
 
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    System.out.println("Thankyou BBye!");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Enter Valid Choice");
+            }
         } while (choice != 6);
-
-        switch (choice) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                System.out.println("Thankyou BBye!");
-             sc.close();
-             return;
-                default :
-                System.out.println("Enter Valid Choice");
-        }
 
     }
 }
