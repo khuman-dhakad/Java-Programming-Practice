dc67aad6e4bdec219b865225a8ade673cb5b49c6
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-03-11 17:54:24 +0530

    Fix calculator logic errors and improve method return implementation

--- Calculator.java
+++ Calculator.java
@@ -1,50 +1,49 @@
-
 import java.util.Scanner;
 
 class Calculator {
+
     public static int calculateSum(int a, int b) {
-        int sum = a + b;
-        return sum;
+        return a + b;
     }
 
     public static int calculateDiff(int a, int b) {
-        int diff = a - b;
-        return diff;
+        return a - b;
     }
 
     public static int calculateMul(int a, int b) {
-        int mul = a * b;
-        return mul;
+        return a * b;
     }
 
     public static float calculateDiv(int a, int b) {
         if (b == 0) {
-            System.out.println("Can Not Divided By 0!");
+            System.out.println("Cannot divide by zero!");
             return 0;
         }
-        float div = (float) a / b;
-        return div;
+        return (float) a / b;
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter First Number:");
+
+        System.out.print("Enter First Number: ");
         int a = sc.nextInt();
-        System.out.print("Enter First Number:");
+
+        System.out.print("Enter Second Number: ");
         int b = sc.nextInt();
 
-        int res = calculateSum(a, b);
-        System.out.println("Addition Is: " + res);
+        int sum = calculateSum(a, b);
+        System.out.println("Addition Is: " + sum);
 
-        int diff = calculateMul(a, b);
-        System.out.println("Diffrence Is: " + diff);
+        int diff = calculateDiff(a, b);
+        System.out.println("Difference Is: " + diff);
 
-        int mul = calculateDiff(a, b);
+        int mul = calculateMul(a, b);
         System.out.println("Multiplication Is: " + mul);
 
         float div = calculateDiv(a, b);
         System.out.println("Division Is: " + div);
 
+        sc.close();
     }
 }
