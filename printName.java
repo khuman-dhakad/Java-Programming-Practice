82311345155f0ab0a1dd170bf1b52632069da3a0
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-19 17:01:04 +0530

    Create String name array and take size from user

--- printName.java
+++ printName.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 public class printName{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of an array : ");
+        int size = sc.nextInt();
+          sc.nextLine();
+
+        String [] name = new String [size];
         
         sc.close();
     }
