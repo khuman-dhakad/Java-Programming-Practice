880219aced76d3029c64387b9036caf44f3932b8
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-19 17:02:53 +0530

    Taking names input using forloop and storing in array

--- printName.java
+++ printName.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 public class printName{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.print("Enter the size of an array : ");
         int size = sc.nextInt();
           sc.nextLine();
 
         String [] name = new String [size];
-        
+         
+         for(int i=0;i<name.length;i++){
+            System.out.print("Enter Name "+(i+1)+" : ");
+            
+            name[i]=sc.nextLine();
+         }
         sc.close();
     }
 }
