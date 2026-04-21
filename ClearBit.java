decd70d48cf3d9bd88e3c215b8b16fad358c9721
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-04-21 18:28:20 +0530

    Added ClearBit program

---
+++ ClearBit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ClearBit{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number : ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter Position : ");
+        int pos = sc.nextInt();
+
+        int bitMask = 1<<pos;
+        int notBitMask = ~(bitMask);
+        int newNumber = num & notBitMask;
+
+        System.out.println(newNumber);
+        sc.close();
+    }
+}
