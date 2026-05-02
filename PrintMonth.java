549b358e0bc7d3ab0bc48890ef3b39d56b1a6373
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-02 11:27:59 +0530

    Added PrintMonth program

---
+++ PrintMonth.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class PrintMonth{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
