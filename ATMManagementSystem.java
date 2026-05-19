7cb4e81bc43307205d8410f3e9fa15bbc5a1d9bd
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-19 20:29:24 +0530

    Added ATM Managment menu driven program

---
+++ ATMManagementSystem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ATMManagementSystem {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter Your Choice : ");
+
+            int choice = sc.nextInt();
+
+    }
+}
