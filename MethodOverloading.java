bf121ce39530cce590de138924625bba6034fd41
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-30 13:02:59 +0530

    Add overloaded add method with three parameters

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -4,7 +4,13 @@ public class MethodOverloading {
         return a + b;
     }
 
+    // New overloaded method
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
     public static void main(String[] args) {
         System.out.println(add(10, 20));
+        System.out.println(add(10, 20, 30));
     }
 }
