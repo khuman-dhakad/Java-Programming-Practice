6a26262e2943f0e29cf04767e9e649cd02f2b9be
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-05-30 13:08:14 +0530

    Add String type overloaded method

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -11,10 +11,14 @@ public class MethodOverloading {
     public static double add(double a, double b) {
         return a + b;
     }
+    public static String add(String a, String b) {
+        return a + b;
+    }
 
     public static void main(String[] args) {
         System.out.println(add(10, 20));
         System.out.println(add(10, 20, 30));
         System.out.println(add(10.5, 20.5));
+        System.out.println(add("Hello ", "Khuman"));
     }
 }
